892acebdd16caddc31be1731d17adfb8576669ac
A.v.s.Pavan <avspavan1234@gmail.com>
2023-10-20 16:18:42 -0500

    Implemented  Bound service, crash fixes

--- app/src/main/java/com/kevin/androidservicesdemo/BoundService.kt
+++ app/src/main/java/com/kevin/androidservicesdemo/BoundService.kt
@@ -14,16 +14,35 @@ class BoundService : Service() {
     override fun onCreate() {
         super.onCreate()
         myBinder = MyBinder()
+        Log.e(  "BoundService", "onCreate: ")
     }
 
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        Log.e("BoundService", "onStartCommand: ")
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+
+
     override fun onBind(intent: Intent?): IBinder? {
+        Log.e("BoundService", "onBind: ")
         return myBinder
     }
 
     override fun onUnbind(intent: Intent?): Boolean {
+        Log.e("BoundService", "onUnbind: ")
         return true
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.e("BoundService", "onDestroy: ")
+    }
+
+    override fun onRebind(intent: Intent?) {
+        super.onRebind(intent)
+        Log.e("BoundService", "onRebind: ")
+    }
 
     fun start() {
         Log.e("BoundService", "start: ")
--- app/src/main/java/com/kevin/androidservicesdemo/MainActivity.kt
+++ app/src/main/java/com/kevin/androidservicesdemo/MainActivity.kt
@@ -6,7 +6,9 @@ import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.IBinder
+import android.util.Log
 import com.kevin.androidservicesdemo.databinding.ActivityMainBinding
+import java.lang.Thread.sleep
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,17 +24,27 @@ class MainActivity : AppCompatActivity() {
     private var isBound = false
     private var boundService: BoundService? = null
 
+    // Intent for Bound Service
+    private val boundServiceIntent by lazy { Intent(this, BoundService::class.java) }
 
+
+    // Service Connection
     private val boundServiceConnection = object : ServiceConnection {
         override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            // Cast IBinder to MyBinder
             val binder = service as BoundService.MyBinder
+            // Get Service
             boundService = binder.getService()
+            // Set isBound to true
             isBound = true
+            binding.randomTxt.text = "Service is bound"
+            Log.e("MainActivity", "onServiceConnected: ")
         }
 
         override fun onServiceDisconnected(name: ComponentName?) {
             isBound = false
             boundService = null
+            Log.e("MainActivity", "onServiceDisconnected: ")
         }
 
     }
@@ -42,15 +54,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        // Bind Service
-        val intent = Intent(this, BoundService::class.java)
-        bindService(intent, boundServiceConnection, BIND_AUTO_CREATE)
-
-
         // Init Views
         initViews()
-
-
     }
 
     // Init Views
@@ -66,19 +71,33 @@ class MainActivity : AppCompatActivity() {
 
         // Bind and Unbind Service
         binding.startBoundBtn.setOnClickListener {
-            boundService?.start()
-            if (isBound) {
-                binding.randomTxt.text = "Service is bound"
-            } else {
-                binding.randomTxt.text = "Service is not bound"
-            }
+            // Bind Service
+            bindService(boundServiceIntent, boundServiceConnection, BIND_AUTO_CREATE)
+
         }
 
 
         // Fetch Random Number
         binding.fetchRandomBtn.setOnClickListener {
+            // check if service is bound
+            if (!isBound) {
+                binding.randomTxt.text = "Service is not bound"
+                return@setOnClickListener
+            }
             binding.randomTxt.text =
                 "Random number from service is ${boundService?.getRandomNumber().toString()}"
+        }
+
+        // Unbind Service
+        binding.unbindBtn.setOnClickListener {
+            // check if service is bound
+            if (!isBound) {
+               binding.randomTxt.text = "Service is not bound/already unbound"
+                return@setOnClickListener
+            }
+            unbindService(boundServiceConnection)
+            isBound = false
+            binding.randomTxt.text = "Service is unbound"
         }
     }
 
